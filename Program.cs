8b727bcfa17927ff08b033b1296375a82a24f43b
gabriwls <gabriwlalves@hotmail.com>
2022-09-26 18:22:13 -0300

    define os testes com as classes Nokia e iPhone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "937459254", modelo: "5.4", imei: "111111111", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "958673492", modelo: "Pro Max 13", imei: "222222222", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
